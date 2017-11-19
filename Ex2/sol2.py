96afb529e6306434f5c4cc9cbbda5664acc2966d
tomerel <tomer@eliyahu.com>
2017-11-19 12:20:50 +0200

    fix kernel problem(*instead+)

--- Ex2/sol2.py
+++ Ex2/sol2.py
@@ -61,7 +61,7 @@ def IDFT2(fourier_image):
     return np.dot(tmp_rows,idft_matrix(cols))/(rows*cols)
 
 def conv_der(im):
-    conv = np.array([[1,0.-1]])
+    conv = np.array([[1,0.-1]])*0.5
     dx = convolve2d (im,conv,mode = "same")
     conv = conv.T
     dy = convolve2d(im,conv, mode = "same")
@@ -93,9 +93,9 @@ def calc_kernel (kernel_size):
         a = np.pad(kernel, ((kernel_size // 2, kernel_size // 2), (0, 0)), mode="constant")
         b = np.pad(kernel.T, ((0, 0), (kernel_size // 2, kernel_size // 2)), mode="constant")
         print ("______________________")
-        kernel = a + b
+        kernel = convolve2d(a,b,mode = "same")
         kernel_sum = kernel.sum()
-        print(kernel_sum)
+        print(kernel)
         kernel = kernel / kernel_sum
 
     else:
@@ -113,21 +113,22 @@ def blur_fourier (im, kernel_size):
     kernel = calc_kernel(kernel_size)
     k_r, k_c = kernel.shape
     s_kernel = np.zeros(im.shape)
-    s_kernel[cols//2-k_r//2:cols//2+k_r][rows//2] = kernel
+    s_kernel[cols//2-k_r//2:cols//2+k_r][rows//2-k_] = kernel
     kernel_F = np.fft.fft2(kernel)
 
 # print(calc_kernel())
 add = "../Ex1/ss.jpg"
 my_pic = read_image(add,1)
-my = blur_fourier(my_pic,11)
+# my = blur_fourier(my_pic,11)
 # my = fourier_der(my)
 #
-plt.imshow(my,cmap="gray")
-plt.show()
-# conv_der(my_pic)
-# fourier_der(my_pic)
-# plt.imshow(my_pic,cmap="gray")
+# plt.imshow(my,cmap="gray")
 # plt.show()
+bs = blur_spatial(my_pic,5)
+c = conv_der(my_pic)
+f = fourier_der(my_pic)
+plt.imshow(bs,cmap="gray")
+plt.show()
 # a = dft_matrix(5)
 # b = np.asarray([5, 1, 2, 3, 4]).reshape(5, 1)
 # print(a)
