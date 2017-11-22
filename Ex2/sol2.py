bfa19bd6ab24c0a39c70959c1415cd65d2bbf0ee
tomerel <tomer@eliyahu.com>
2017-11-22 18:58:09 +0200

    ex2 DONE

--- Ex2/sol2.py
+++ Ex2/sol2.py
@@ -28,28 +28,34 @@ def read_image(filename, representation):
 
 
 def dft_matrix(N):
+    """:return the n*n dft matrix"""
     exp_const = -np.pi * 2j / N
     matrix = np.fromfunction(lambda x, u: np.exp(exp_const * (x * u)), (N, N), dtype=np.complex128)
     return matrix
 
 
 def idft_matrix(N):
+    """:return the n*n idft matrix"""
     exp_const = np.pi * 2j / N
     matrix = np.fromfunction(lambda x, u: np.exp(exp_const * (x * u)), (N, N), dtype=np.complex128)
     return matrix
 
 
 def DFT(signal):
+    """transform a 1D discrete signal to its Fourier representation"""
     return np.dot(dft_matrix(signal.shape[0]), signal)
 
 
 def IDFT(fourier_signal):
+    """ performs inverse DFT - transform a 1D Fourier representation to discrete signal """
+
     N = len(fourier_signal)
-    print(N)
+    # print(N)
     return np.dot(idft_matrix(fourier_signal.shape[0]), fourier_signal / N)
 
 
 def DFT2(image):
+    """convert a 2D discrete signal to its Fourier representation"""
     rows, cols = image.shape
     tmp_rows = np.dot(dft_matrix(rows), image)
 
@@ -57,6 +63,7 @@ def DFT2(image):
 
 
 def IDFT2(fourier_image):
+    """performs inverse DFT - convert a 2D Fourier representation to discrete signal """
     rows, cols = fourier_image.shape
     tmp_rows = np.dot(idft_matrix(rows), fourier_image)
 
@@ -64,6 +71,7 @@ def IDFT2(fourier_image):
 
 
 def conv_der(im):
+    """computes the magnitude of image derivatives"""
     conv = np.array([[1, 0. - 1]]) ## TODO * 0.5
     dx = convolve2d(im, conv, mode="same")
     conv = conv.T
@@ -71,11 +79,12 @@ def conv_der(im):
     # plt.imshow(dy, cmap="gray")
 
     magnitude = np.sqrt(np.abs(dx) ** 2 + np.abs(dy) ** 2)
-    print(magnitude)
+    # print(magnitude)
     return magnitude
 
 
 def fourier_der(im):
+    """computes the magnitude of image derivatives using Fourier transform"""
     rows, cols = im.shape
     exp_const_x = np.pi * 2j / rows
     exp_const_y = np.pi * 2j / cols
@@ -91,33 +100,40 @@ def fourier_der(im):
 
 
 def calc_kernel(kernel_size):
-    base_kernel = np.array([[1, 1]])
-    kernel = base_kernel
-    if (kernel_size > 1 and kernel_size % 2 == 1):
-        for i in range(kernel_size - 2):
-            kernel = convolve2d(kernel, base_kernel)
-        a = np.pad(kernel, ((kernel_size // 2, kernel_size // 2), (0, 0)), mode="constant")
-        b = np.pad(kernel.T, ((0, 0), (kernel_size // 2, kernel_size // 2)), mode="constant")
-        print("______________________")
-        kernel = convolve2d(a, b, mode="same")
-        kernel_sum = kernel.sum()
-        print(kernel)
-        kernel = kernel / kernel_sum
+    """returns approximation of the gaussian kernel using the binomial coefficients
+    with shape (kernel_size, kernel_size)in image space """
+    if (kernel_size == 1):
+        return np.array([[1]])
+    base_kernel = np.array([1, 1])
+    kernel = np.array([1, 1],np.float64)
+    for i in range(kernel_size - 2):
+        kernel = np.convolve(kernel, base_kernel)
+    kernel = kernel / kernel.sum()
+    # a = np.pad(kernel, ((kernel_size // 2, kernel_size // 2), (0, 0)), mode="constant")
+    # b = np.pad(kernel.T, ((0, 0), (kernel_size // 2, kernel_size // 2)), mode="constant")
+    # print("______________________")
+    kernel = np.matrix([kernel])
+    kernel = kernel.T*kernel
 
-    else:
-        raise ValueError
+    # kernel_sum = kernel.sum()
+    # print(kernel)
+    # kernel = kernel / kernel_sum
+
     return kernel
 
 
 def blur_spatial(im, kernel_size):
+    """performs image blurring using 2D convolution between the image im and a gaussian
+    kernel with size kernel_size*kernel_size."""
     kernel = calc_kernel(kernel_size)
-    print(kernel)
+    # print(kernel)
     return convolve2d(im, kernel, mode="same")
     # print (b)
     # print(np.add(a,b))
 
 
 def blur_fourier(im, kernel_size):
+    """performs image blurring with gaussian kernel in Fourier space."""
     rows, cols = im.shape
     kernel = calc_kernel(kernel_size)
     k_r, k_c = kernel.shape
@@ -127,14 +143,14 @@ def blur_fourier(im, kernel_size):
     c = not cols % 2
     padded_kernel = np.pad(kernel, (((rows - k_r) // 2, (rows - k_r) // 2 + r),
                                     ((cols - k_c) // 2, (cols - k_c) // 2 + c)), mode="constant")
-    print("im =  ", im.shape)
-    print("kernel = ", padded_kernel.shape)
+    # print("im =  ", im.shape)
+    # print("kernel = ", padded_kernel.shape)
     kernel_F = DFT2(padded_kernel)
     im_F = DFT2(im)
     blur_F = np.multiply(im_F, kernel_F)
-    return np.fft.ifftshift(IDFT2(blur_F))
-
-# print(calc_kernel())
+    return np.real(np.fft.ifftshift(IDFT2(blur_F)))
+#
+# print(calc_kernel(1).shape)
 # add = "../Ex1/ss.jpg"
 # my_pic = read_image(add, 1)
 # my = blur_fourier(my_pic,11)
